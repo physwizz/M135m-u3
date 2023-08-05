2fc70c556bf502aed623dd879a6c51b4aa28a79f
physwizz <pollockg86@gmail.com>
2023-08-05 10:32:31 +1000

    fix integrity

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,5 +4,7 @@ export ARCH=arm64
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 
-make ARCH=arm64 exynos850-m13ins_defconfig
+make ARCH=arm64 physwizz_defconfig
 make ARCH=arm64 -j64
+
+
